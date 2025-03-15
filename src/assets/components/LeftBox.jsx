8a1b0a67afafc6eae16bb87c2d7f43c371f3ce2c
laylasheikh <laylasheikh9@gmail.com>
2025-03-15 14:12:53 -0400

    add line

--- src/assets/components/LeftBox.jsx
+++ src/assets/components/LeftBox.jsx
@@ -4,11 +4,15 @@ function LeftBox({ isClicked }) {
             <p>{isClicked}</p>
             <p>hello</p>
 
+            <div class="line">
+
             <div class="button-path">
                 <button class="button"></button>
                 <button class="button"></button>
                 <button class="button"></button>
             </div>
+            </div>
+
         </div>
     )
 }
