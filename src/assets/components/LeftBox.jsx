836df82acc0b98a459c94c7b62aab9ce1b8cbea5
laylasheikh <laylasheikh9@gmail.com>
2025-03-15 14:49:55 -0400

    buttons more

--- src/assets/components/LeftBox.jsx
+++ src/assets/components/LeftBox.jsx
@@ -1,12 +1,16 @@
 function LeftBox({ isClicked }) {
     return (
         <div class = "leftBoxDiv">
-            <p>{isClicked}</p>
-            <p>hello</p>
+            <p>{isClicked}</p>s
 
             <div class="line">
-
             <div class="button-path">
+                <button class="button"></button>
+                <button class="button"></button>
+                <button class="button"></button>
+                <button class="button"></button>
+                <button class="button"></button>
+                <button class="button"></button>
                 <button class="button"></button>
                 <button class="button"></button>
                 <button class="button"></button>
