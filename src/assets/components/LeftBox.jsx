3adface624a3f086e0c15707cb376c4c80207aad
laylasheikh <laylasheikh9@gmail.com>
2025-03-15 14:02:11 -0400

    add some buttons

--- src/assets/components/LeftBox.jsx
+++ src/assets/components/LeftBox.jsx
@@ -3,6 +3,12 @@ function LeftBox({ isClicked }) {
         <div>
             <p>{isClicked}</p>
             <p>hello</p>
+
+            <div class="button-path">
+                <button class="button"></button>
+                <button class="button"></button>
+                <button class="button"></button>
+            </div>
         </div>
     )
 }
