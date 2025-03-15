82666ec71da14bd036437f707643ea13607294fd
laylasheikh <laylasheikh9@gmail.com>
2025-03-15 14:55:49 -0400

    add labels

--- src/assets/components/LeftBox.jsx
+++ src/assets/components/LeftBox.jsx
@@ -4,17 +4,31 @@ function LeftBox({ isClicked }) {
             <p>{isClicked}</p>s
 
             <div class="line">
-            <div class="button-path">
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
-                <button class="button"></button>
+                <div class="button-path">
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                    <button class="button"></button>
+                </div>
             </div>
+
+            <div class = "labels">
+                <ul class = "stopNames">
+                    <li>Arlington</li>
+                    <li>Copley</li>
+                    <li>Prudential</li>
+                    <li>Symphony</li>
+                    <li>Northeastern</li>
+                    <li>Museum of Fine Arts</li>
+                    <li>Longwood Medical Area</li>
+                    <li>Brigham Circle</li>
+                    <li>Fenwood Road</li>
+                </ul>
             </div>
 
         </div>
