f86a6c03baed02a7d6d9e85e51ba4a1e394644ce
Dreshta Boghra <boghra.d@northeastern.edu>
2025-03-17 00:02:10 -0400

    Update RightBox.jsx in the edits branch

--- src/assets/components/RightBox.jsx
+++ src/assets/components/RightBox.jsx
@@ -1,26 +1,137 @@
-function RightBox ({isClicked}) {
-    if (isClicked) {
-        return (
-            <div>
-                <p>You clicked me!</p>
-                <p>right box</p>
-            </div>
-        )
-    }
-    return (
-        <div class = "blogList">
-            <div class ="blogBox">
-                <h2>Blog Post Title Placeholder</h2>
-                <p>post info blah blah ..................................</p>
-            </div>
-            <div class ="blogBox">
-                <h2>Blog Post Title Placeholder</h2>
-                <p>post info blah blah ..................................</p>
-            </div>
+function RightBox({ stop }) {
+  switch (stop) {
+    case "Arlington":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
         </div>
-    
-        
-    )
+      );
+      case "Copley":
+        return (
+          <div class="blogList">
+            <div class="blogBox">
+              <h2>Blog Post Title Placeholder</h2>
+              <p>post info blah blah ..................................</p>
+            </div>
+            <div class="blogBox">
+              <h2>Blog Post Title Placeholder</h2>
+              <p>post info blah blah ..................................</p>
+            </div>
+          </div>
+        );
+        case "Prudential":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Symphony":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Northeastern":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Museum of Fine Arts":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Longwood Medical Area":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Brigham Circle":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+      case "Fenwood Road":
+      return (
+        <div class="blogList">
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+          <div class="blogBox">
+            <h2>Blog Post Title Placeholder</h2>
+            <p>post info blah blah ..................................</p>
+          </div>
+        </div>
+      );
+    default:
+        return (
+            <div class="blogList">
+            <div class="blogBox">
+                <h2>hi</h2>
+                <p>post info blah blah ..................................</p>
+            </div>
+            <div class="blogBox">
+                <h2>Blog Post Title Placeholder</h2>
+                <p>post info blah blah ..................................</p>
+            </div>
+            </div>
+        );
+
+  }
 }
 
 export default RightBox;
